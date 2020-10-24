7b80a987b3fcc12bb545bbfaf69d05f6f2ccd765
SamuelRaja2712 <71319314+SamuelRaja2712@users.noreply.github.com>
2020-10-24 21:14:42 +0530

    Update Remove External User.ps1

--- Remove External User.ps1
+++ Remove External User.ps1
@@ -1,4 +1,3 @@
-  
 $scriptdirectory = "D:\Powershell Scripts\"
 Import-Module $($scriptdirectory + '\Reference\Microsoft.Online.SharePoint.PowerShell')
 Add-Type -Path $($scriptdirectory + '\Reference\Microsoft.Online.SharePoint.PowerShell\Microsoft.SharePoint.Client.dll')
@@ -6,8 +5,10 @@ Add-Type -Path $($scriptdirectory + '\Reference\Microsoft.Online.SharePoint.Powe
 Add-Type -Path $($scriptdirectory + '\Reference\Microsoft.Online.SharePoint.PowerShell\Microsoft.Online.SharePoint.Client.Tenant.dll')
 #$Credentials = Get-Credential
 $credfile = Import-Csv $($scriptdirectory + "SharePoint\Credentials\credentials.csv")
+#get username and password from credentials csv
 $userName = $credfile[0].UserName
 $password = ConvertTo-SecureString $credfile[0].Password
+#Initializing Admin site URL
 $AdminSiteURL="https://jsrsp-admin.sharepoint.com/"
 #$cred = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($userName,$password)
 $cred = New-Object System.Management.Automation.PSCredential($userName,$password)
