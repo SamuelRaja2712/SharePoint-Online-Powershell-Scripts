e161275255e0552131594a0b3048e05a80d3aba2
SamuelRaja2712 <71319314+SamuelRaja2712@users.noreply.github.com>
2020-10-24 21:12:26 +0530

    Update Delete SPO Site Collection Permanently.ps1

--- Delete SPO Site Collection Permanently.ps1
+++ Delete SPO Site Collection Permanently.ps1
@@ -1,12 +1,16 @@
-﻿$scriptdirectory = "D:\Powershell Scripts\"
+$scriptdirectory = "D:\Powershell Scripts\"
 Import-Module $($scriptdirectory + '\Reference\Microsoft.Online.SharePoint.PowerShell')
 Add-Type -Path $($scriptdirectory + '\Reference\Microsoft.Online.SharePoint.PowerShell\Microsoft.SharePoint.Client.dll')
 Add-Type -Path $($scriptdirectory + '\Reference\Microsoft.Online.SharePoint.PowerShell\Microsoft.SharePoint.Client.Runtime.dll')
 Add-Type -Path $($scriptdirectory + '\Reference\Microsoft.Online.SharePoint.PowerShell\Microsoft.Online.SharePoint.Client.Tenant.dll')
 #$Credentials = Get-Credential
 $credfile = Import-Csv $($scriptdirectory + "SharePoint\Credentials\credentials.csv")
+#get username and password from credentials csv
 $userName = $credfile[0].UserName
 $password = ConvertTo-SecureString $credfile[0].Password
+#Enter Site URL to delete permanently
 $siteURL = Read-Host "Enter Site URL"
+#Delete site from Active sites list in admin center
 Remove-SPOSite -Identity $siteURL -Confirm
+#Delete site from Deleted sites list in admin center
 Remove-SPODeletedSite -Identity $siteURL -Confirm
