94b27233e9f92408a950f5ba31e382d175431f1c
SamuelRaja2712 <71319314+SamuelRaja2712@users.noreply.github.com>
2020-10-24 21:05:36 +0530

    Add files via upload

--- Delete SPO Site Collection Permanently.ps1
+++ Delete SPO Site Collection Permanently.ps1
@@ -7,12 +7,6 @@ Add-Type -Path $($scriptdirectory + '\Reference\Microsoft.Online.SharePoint.Powe
 $credfile = Import-Csv $($scriptdirectory + "SharePoint\Credentials\credentials.csv")
 $userName = $credfile[0].UserName
 $password = ConvertTo-SecureString $credfile[0].Password
-$AdminSiteURL="https://jsrsp-admin.sharepoint.com/"
-$SiteURL="https://jsrsp.sharepoint.com//sites/spsite"
-#$cred = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($userName,$password)
-$cred = New-Object System.Management.Automation.PSCredential($userName,$password)
-$UserID = Read-Host "Enter External User Email Address"
-Connect-SPOService $AdminSiteURL -Credential $cred
-
-$user = Get-SPOExternalUser -Filter $UserID
-Remove-SPOExternalUser -UniqueIDs @($user.UniqueId)
+$siteURL = Read-Host "Enter Site URL"
+Remove-SPOSite -Identity $siteURL -Confirm
+Remove-SPODeletedSite -Identity $siteURL -Confirm
